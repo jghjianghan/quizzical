348be3f90387f87c6bc5748ef5d9beedeaf0765c
Jiang Han <jianghan99@gmail.com>
2022-09-04 15:24:05 +0800

    Displays quiz entries, functional buttons

--- src/pages/quiz/QuizEntry.jsx
+++ src/pages/quiz/QuizEntry.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import OptionButton from "./OptionButton";
+import OptionState from "./OptionState";
 
 function QuizEntry(props) {
   const { question, options, selectOptionHandler } = props;
+
   return (
-    <div>
+    <div className="quiz-entry">
       <h1>{question}</h1>
-      {options && options.map((option) => <button>{option}</button>)}
+      <div className="option-container">
+        {options &&
+          options.map((option) => (
+            <OptionButton
+              key={option.value}
+              text={option.value}
+              optionState={option.state}
+              clickHandler={() => selectOptionHandler(option.value)}
+            />
+          ))}
+      </div>
+      <hr />
     </div>
   );
 }
--- src/pages/quiz/QuizPage.jsx
+++ src/pages/quiz/QuizPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import OptionState from "./OptionState";
 import QuizEntry from "./QuizEntry";
+// import { Buffer } from "buffer";
 
 function shuffleArray(array) {
   let currentIndex = array.length,
@@ -21,33 +23,123 @@ function shuffleArray(array) {
   return array;
 }
 
+function decodeData(str) {
+  return decodeURIComponent(str);
+}
+
 function QuizPage() {
   const [quizes, setquizes] = useState();
+  const [isChecked, setIsChecked] = useState(false);
+  const [score, setScore] = useState();
 
   useEffect(() => {
-    const apiURL = "https://opentdb.com/api.php?amount=10";
+    const apiURL =
+      "https://opentdb.com/api.php?amount=5&encode=url3986&type=multiple";
     fetch(apiURL)
       .then((res) => res.json())
       .then((data) => {
         setquizes(
-          data.results.map((result) => ({
-            question: result.question,
-            correctAnswer: result.correct_answer,
-            answers: shuffleArray([
-              result.correct_answer,
-              ...result.incorrect_answers,
-            ]),
-          }))
+          data.results.map((result) => {
+            const correctAnswer = decodeData(result.correct_answer);
+            const shuffledOptions = shuffleArray([
+              correctAnswer,
+              ...result.incorrect_answers.map((ans) => decodeData(ans)),
+            ]);
+            return {
+              question: decodeData(result.question),
+              correctAnswer,
+              answers: shuffledOptions.map((option) => ({
+                state: OptionState.NOT_SELECTED,
+                value: option,
+              })),
+              selectedAnswer: "",
+            };
+          })
         );
       });
   }, []);
 
-  console.log(quizes);
+  function answerSelected(quizNo, answer) {
+    if (isChecked) return;
+
+    setquizes((prev) =>
+      prev.map((quiz, idx) => {
+        if (idx == quizNo) {
+          quiz.selectedAnswer = answer;
+          quiz.answers = quiz.answers.map((qanswer) =>
+            qanswer.value === answer
+              ? {
+                  ...qanswer,
+                  state: OptionState.SELECTED,
+                }
+              : {
+                  ...qanswer,
+                  state: OptionState.NOT_SELECTED,
+                }
+          );
+        }
+        return quiz;
+      })
+    );
+  }
+
+  function checkAnswers() {
+    if (isChecked) return;
+
+    let countScore = 0;
+    setIsChecked(true);
+
+    for (let quiz of quizes) {
+      if (quiz.correctAnswer === quizes.selectedAnswer) {
+        countScore++;
+      }
+    }
+
+    setScore(countScore);
+
+    setquizes((prev) =>
+      prev.map((quiz) => {
+        quiz.answers = quiz.answers.map((qanswer) => {
+          const newAnswer = { ...qanswer };
+          switch (newAnswer.state) {
+            case OptionState.SELECTED:
+              if (newAnswer.value === quiz.correctAnswer) {
+                newAnswer.state = OptionState.CHECKED_CORRECT;
+              } else {
+                newAnswer.state = OptionState.CHECKED_WRONG;
+              }
+              break;
+            case OptionState.NOT_SELECTED:
+              if (newAnswer.value === quiz.correctAnswer) {
+                newAnswer.state = OptionState.NOT_SELECTED_CORRECT;
+              } else {
+                newAnswer.state = OptionState.CHECKED_NOT_SELECTED;
+              }
+              break;
+          }
+          return newAnswer;
+        });
+
+        return quiz;
+      })
+    );
+  }
 
   return (
     <div className="quiz-container">
-      <QuizEntry />
-      <button id="submit-button">Check answers</button>
+      {quizes &&
+        quizes.map((quiz, idx) => (
+          <QuizEntry
+            key={quiz.question}
+            question={quiz.question}
+            options={quiz.answers}
+            selectOptionHandler={(answer) => answerSelected(idx, answer)}
+          />
+        ))}
+
+      <button id="submit-button" onClick={checkAnswers}>
+        Check answers
+      </button>
     </div>
   );
 }
